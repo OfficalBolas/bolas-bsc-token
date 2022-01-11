aeb115e660394c031ecd360877e4c1340c43815b
Nipuna Sudharaka <nipuna.sudha@gmail.com>
2022-01-11 14:10:33 +0530

    Remove unused import

--- contracts/BOLAS.sol
+++ contracts/BOLAS.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.2;
 
 // OpenZeppelin libs
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 // UniSwap libs
